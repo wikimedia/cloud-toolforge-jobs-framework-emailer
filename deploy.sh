9f0f71c2a24b53ca6959ae73855fe1586dd9f585
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2023-07-10 14:41:39 +0200

    jobs-framework-emailer: move to gitlab
    Move repository to gitlab.
    
    Bug: T341084
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>
    Change-Id: I5baa863a946b40113b82717736cf7425717b9b72

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# see https://phabricator.wikimedia.org/T303931
-
-where=$(cat /etc/wmcs-project 2>/dev/null || echo "devel-kind")
-kubectl apply -k deployment/${where}
--- emailer.py
+++
@@ -1,4 +0,0 @@
-import runpy
-
-if __name__ == "__main__":
-    runpy.run_module("emailer", run_name="__main__")
